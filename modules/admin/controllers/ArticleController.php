243f2d69ea4b7f4f3b72b613df632e0f9d3950c6
mestnq <mestnq@mail.ru>
2023-04-10 01:42:06 +0500

    added set tags

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -6,6 +6,7 @@ use app\models\Article;
 use app\models\ArticleSearch;
 use app\models\Category;
 use app\models\ImageUpload;
+use app\models\Tag;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\web\Controller;
@@ -157,6 +158,25 @@ class ArticleController extends Controller
             'article'=>$article,
             'selectedCategory'=>$selectedCategory,
             'categories'=>$categories
+        ]);
+    }
+
+    public function actionSetTags($id)
+    {
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags();
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+
+        if(Yii::$app->request->isPost)
+        {
+            $tags = Yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id'=>$article->id]);
+        }
+
+        return $this->render('tags', [
+            'selectedTags'=>$selectedTags,
+            'tags'=>$tags
         ]);
     }
 
