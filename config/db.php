9ea08fa2032d761905918cae6313169cdb8794ec
mestnq <mestnq@mail.ru>
2023-04-09 18:33:53 +0500

    added yii2 and db

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;port=3306;dbname=my_database_name',
+    'username' => 'my_user_name',
+    'password' => 'my_user_password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
